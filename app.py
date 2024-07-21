6856018f83f750255910b6879d805d3167c0031d
Michael Zhang <m468zhan@uwaterloo.ca>
2024-07-21 02:24:27 -0400

    update heroku

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from flask_cors import CORS
 from src.main import create_app
 
 app = create_app()
-CORS(app)
+CORS(app, origins="http://localhost:3000", supports_credentials=True, methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
